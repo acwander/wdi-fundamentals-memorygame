414793e55898928982f5c89bfbecb6f48eddb42d
Alex Wander <alexcwander@gmail.com>
2020-04-18 18:27:59 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,20 +1,23 @@
-// Cards Array
-let cards = ["queen", "queen", "king", "king"];
-let cardsInPlay = [];
+// Arrays
+let cards = ["queen", "queen", "king", "king"]; // All available cards
+let cardsInPlay = []; // Empty array for storing pulled cards
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
+// Functions
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.");
+    }
   }
 }
+
+function flipCard(cardID) {
+  console.log("User flipped " + cards[cardID]);
+  cardsInPlay.push(cards[cardID]);
+  checkForMatch();
+}
+// Call functions
+flipCard(0);
+flipCard(1);
