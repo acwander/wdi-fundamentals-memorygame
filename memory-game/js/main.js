6207b37847c7b7c2d80191645db5a64e30239f86
Alex Wander <alexcwander@gmail.com>
2020-04-19 15:14:28 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,34 @@
 // Arrays
-let cards = ["queen", "queen", "king", "king"]; // All available cards
-let cardsInPlay = []; // Empty array for storing pulled cards
+
+// All available cards objects
+let cards = [
+  {
+    rank: "queen",
+    suite: "hearts",
+    cardImage: "images/queen-of-hearts.png",
+  },
+  {
+    rank: "queen",
+    suite: "diamonds",
+    cardImage: "images/queen-of-diamonds.png",
+  },
+  {
+    rank: "king",
+    suite: "hearts",
+    cardImage: "images/king-of-hearts.png",
+  },
+  {
+    rank: "king",
+    suite: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+  },
+];
+
+// Empty array for storing pulled cards
+let cardsInPlay = [];
 
 // Functions
+// Check for two cards matching fucntion
 function checkForMatch() {
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -13,11 +39,15 @@ function checkForMatch() {
   }
 }
 
-function flipCard(cardID) {
-  console.log("User flipped " + cards[cardID]);
-  cardsInPlay.push(cards[cardID]);
+// Flip card function
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].suite);
+  console.log(cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 }
+
 // Call functions
 flipCard(0);
-flipCard(1);
+flipCard(2);
